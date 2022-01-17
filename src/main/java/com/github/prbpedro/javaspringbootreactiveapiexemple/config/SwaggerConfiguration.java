4d5bb04234db0e1c50b8237cdfa47bb960628452
Pedro Ribeiro Baptista <prbpedro@gmail.com>
2022-01-17 15:23:37 -0300

    1

--- src/main/java/com/github/prbpedro/javaspringbootreactiveapiexemple/config/SwaggerConfiguration.java
+++ src/main/java/com/github/prbpedro/javaspringbootreactiveapiexemple/config/SwaggerConfiguration.java
@@ -12,7 +12,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-@SuppressWarnings("unused")
 class SwaggerConfiguration {
     @Bean
     static Docket api() {
